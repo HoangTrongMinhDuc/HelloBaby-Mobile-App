78c3cc5f54b918cf1b4136b3c86e292bf67c97b2
trandinhquy97 <trandinhquy97@gmail.com>
2017-09-16 10:00:43 +0700

    thêm  bản đồ ...

--- app/src/main/java/com/example/admin/xmltest/ChucNang3Frag.java
+++ app/src/main/java/com/example/admin/xmltest/ChucNang3Frag.java
@@ -1,12 +1,16 @@
 package com.example.admin.xmltest;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,15 +27,27 @@ public class ChucNang3Frag extends Fragment implements OnMapReadyCallback {
     private View vView;
     private GoogleMap gMap;
     private ProgressDialog progressDialog;
+    private Button btnFind;
     @Nullable
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         vView=inflater.inflate(R.layout.fragment_chuc_nang3, container, false);
+        getActivity().setTitle("QUẢN LÝ");
         progressDialog =new ProgressDialog(getContext());
         progressDialog.setTitle("Thông báo");
         progressDialog.setMessage("Đang tải bản đồ");
         progressDialog.show();
+
+
+        btnFind= (Button) vView.findViewById(R.id.btnfind);
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("01647723485", null, "sms message", null, null);
+            }
+        });
         return vView;
     }
 
@@ -51,7 +67,12 @@ public class ChucNang3Frag extends Fragment implements OnMapReadyCallback {
         gMap=googleMap;
         gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(20.2,34.2),5));
-        progressDialog.dismiss();
+        gMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                progressDialog.dismiss();
+            }
+        });
 
     }
 }
