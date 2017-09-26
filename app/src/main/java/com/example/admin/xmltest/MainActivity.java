16437b7ad88c254c1453ddda1f9e2475babbce0c
trandinhquy97 <trandinhquy97@gmail.com>
2017-09-26 20:37:03 +0700

    Đăng nhập được với admin1@gmail.com:admin1

--- app/src/main/java/com/example/admin/xmltest/MainActivity.java
+++ app/src/main/java/com/example/admin/xmltest/MainActivity.java
@@ -2,19 +2,29 @@ package com.example.admin.xmltest;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtUserName, edtPassWords;
     Button btnLogin, btnGiaoDuc,btnComic,btnVideo,btnChonlua;
     TextView tvSignUp;
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
         addControls();
         addEvents();
 
@@ -42,16 +53,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //SharedPreferences pre=getSharedPreferences("login_data",MODE_PRIVATE);
-                Intent intent=new Intent(MainActivity.this,NavigationDrawer.class);
-                intent.putExtra("username",edtPassWords.getText().toString());
-                intent.putExtra("password",edtUserName.getText().toString());
+
                 SharedPreferences.Editor edit=pre.edit();
 
                 edit.putString("username",edtPassWords.getText().toString());
                 edit.putString("password",edtUserName.getText().toString());
                 edit.commit();
-                startActivity(intent);
-                finish();
+                
+                xuLyDangNhap();
+
             }
         });
 
@@ -68,6 +78,36 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    private void xuLyDangNhap() {
+        String email=edtUserName.getText().toString();
+        String password=edtPassWords.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Đăng nhập thành công",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent=new Intent(MainActivity.this,NavigationDrawer.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(MainActivity.this, "Đăng nhập không thành công",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 
