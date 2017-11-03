9757104f75c9c67a3a42a7b30d678508936f52b9
trandinhquy97 <trandinhquy97@gmail.com>
2017-11-03 20:00:04 +0700

    GPS + SMS send

--- app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
+++ app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
@@ -10,11 +10,17 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import static android.content.Context.LOCATION_SERVICE;
 import static android.content.Context.MODE_PRIVATE;
@@ -25,38 +31,84 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class SonReceiveAndSendBackService extends BroadcastReceiver {
 
-
     @RequiresApi(api = Build.VERSION_CODES.DONUT)
 
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private double longitude, latitude;
+    private LocationManager mLocationManager;
+    private LocationListener mLocationListener;
+
+    private Location currentBestLocation = null;
+
+
     @Override
     public void onReceive(final Context context, Intent intent) {
-        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Toast.makeText(context, location.getLatitude() +":"+location.getLongitude(), Toast.LENGTH_LONG).show();
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
+//        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+//        locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                Toast.makeText(context, location.getLatitude() +":"+location.getLongitude(), Toast.LENGTH_LONG).show();
+//                latitude = location.getLatitude();
+//                longitude = location.getLongitude();
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String s, int i, Bundle bundle) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderEnabled(String s) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderDisabled(String s) {
+//
+//            }
+//        };
+//        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        else {
+//            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+//        }
+
+
+        Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show();
+
+        Bundle bundle = intent.getExtras();
+        Object[] pdus = (Object[]) bundle.get("pdus");
+        for (int i = 0; i < pdus.length; i++) {
+            SmsMessage smsMessage;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                String format = bundle.getString("format");
+                smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+            } else {
+                smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            }
+            String noidung = smsMessage.getMessageBody();
+            String phone = smsMessage.getOriginatingAddress();
+            Toast.makeText(context, "Số phone=" + phone + "\nNội dung:" + noidung, Toast.LENGTH_LONG).show();
+            if (noidung.indexOf("+hello123") != -1) {
+                currentBestLocation = getLastBestLocation(context);
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phone, null, "+hello123:" + currentBestLocation.getLongitude() + "$" + currentBestLocation.getAltitude(), null, null);
+                //Toast.makeText(context,longitude +"$"+latitude,Toast.LENGTH_LONG).show();
             }
 
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
 
-            }
+    }
 
-            @Override
-            public void onProviderEnabled(String s) {
+    private Location getLastBestLocation(Context context) {
 
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        };
         if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -65,37 +117,27 @@ public class SonReceiveAndSendBackService extends BroadcastReceiver {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return;
+            return null;
+        }
+        Location locationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        Location locationNet = mLocationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) { GPSLocationTime = locationGPS.getTime(); }
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if ( 0 < GPSLocationTime - NetLocationTime ) {
+            return locationGPS;
         }
         else {
-            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            return locationNet;
         }
-
-
-
-
-
-        Bundle bundle = intent.getExtras();
-        Object[] pdus = (Object[]) bundle.get("pdus");
-        for (int i = 1; i < pdus.length; i++) {
-            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
-            String noidung = smsMessage.getMessageBody();
-            String phone = smsMessage.getOriginatingAddress();
-            Toast.makeText(context, "Số phone=" + phone + "\nNội dung:" + noidung, Toast.LENGTH_SHORT).show();
-            if (noidung.indexOf("+hello123") != -1) {
-
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(phone, null, "+hello123:"+longitude+"$"+latitude, null, null);
-                Toast.makeText(context,longitude +"$"+latitude,Toast.LENGTH_LONG).show();
-
-            }
-
-        }
-
-
     }
-
-
 
 
 
