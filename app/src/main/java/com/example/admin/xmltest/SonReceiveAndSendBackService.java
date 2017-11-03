3db1ea731af1e0148ef973e7d0fd87281985bcce
trandinhquy97 <trandinhquy97@gmail.com>
2017-11-03 17:54:15 +0700

    SMS send

--- app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
+++ app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
@@ -4,12 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
+import static android.content.Context.LOCATION_SERVICE;
 import static android.content.Context.MODE_PRIVATE;
 
 /**
@@ -18,23 +24,66 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class SonReceiveAndSendBackService extends BroadcastReceiver {
 
+
     @RequiresApi(api = Build.VERSION_CODES.DONUT)
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Bundle bundle=intent.getExtras();
-        Object[] pdus= (Object[]) bundle.get("pdus");
-        for (int i=1;i<pdus.length;i++)
-        {
-            SmsMessage smsMessage=SmsMessage.createFromPdu((byte[]) pdus[i]);
-            String noidung=smsMessage.getMessageBody();
-            String phone=smsMessage.getOriginatingAddress();
-            Toast.makeText(context,"Số phone="+phone+"\nNội dung:"+noidung,Toast.LENGTH_SHORT).show();
-            if(noidung.indexOf("+hello123")!=-1){
-                Toast.makeText(context,"Đang lấy vị trí...",Toast.LENGTH_LONG).show();
+    public void onReceive(final Context context, Intent intent) {
+        Bundle bundle = intent.getExtras();
+        Object[] pdus = (Object[]) bundle.get("pdus");
+        for (int i = 1; i < pdus.length; i++) {
+            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
+            String noidung = smsMessage.getMessageBody();
+            String phone = smsMessage.getOriginatingAddress();
+            Toast.makeText(context, "Số phone=" + phone + "\nNội dung:" + noidung, Toast.LENGTH_SHORT).show();
+            if (noidung.indexOf("+hello123") != -1) {
+                Toast.makeText(context, "Đang lấy vị trí...", Toast.LENGTH_LONG).show();
             }
+
         }
 
+        locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(context, location.getLatitude() +":"+location.getLongitude(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        else {
+            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        }
     }
+
+
+
 
 
 }
