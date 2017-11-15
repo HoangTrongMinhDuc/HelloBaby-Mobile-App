6c4f87af14ed8c393db3b35f0ec066473d7f828e
trandinhquy97 <trandinhquy97@gmail.com>
2017-11-15 23:20:27 +0700

    GetGPS

--- app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
+++ app/src/main/java/com/example/admin/xmltest/SonReceiveAndSendBackService.java
@@ -17,7 +17,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
+import android.app.Service;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -78,6 +80,27 @@ public class SonReceiveAndSendBackService extends BroadcastReceiver {
 //        else {
 //            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
 //        }
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
 
 
         Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show();
@@ -96,10 +119,43 @@ public class SonReceiveAndSendBackService extends BroadcastReceiver {
             String noidung = smsMessage.getMessageBody();
             String phone = smsMessage.getOriginatingAddress();
             Toast.makeText(context, "Số phone=" + phone + "\nNội dung:" + noidung, Toast.LENGTH_LONG).show();
+
+
             if (noidung.indexOf("+hello123") != -1) {
-                currentBestLocation = getLastBestLocation(context);
-                SmsManager smsManager = SmsManager.getDefault();
-                smsManager.sendTextMessage(phone, null, "+hello123:" + currentBestLocation.getLongitude() + "$" + currentBestLocation.getAltitude(), null, null);
+//                currentBestLocation = getLastBestLocation(context);
+                mLocationManager = (LocationManager) context
+                        .getSystemService(Context.LOCATION_SERVICE);
+                    if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+                    boolean isNetworkEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+                    if(isGPSEnabled)
+                        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000, 5, mLocationListener);
+                    else if(isNetworkEnabled)
+                         mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000,5,mLocationListener);
+                    Location gotLoc = mLocationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if(gotLoc!=null)
+                        currentBestLocation=gotLoc;
+                    Toast.makeText(context, "Mobile Location (NW):"+currentBestLocation,
+                            Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+
+                //SmsManager smsManager = SmsManager.getDefault();
+                //smsManager.sendTextMessage(phone, null, "+hello123:" + currentBestLocation.getLongitude() + "$" + currentBestLocation.getAltitude(), null, null);
                 //Toast.makeText(context,longitude +"$"+latitude,Toast.LENGTH_LONG).show();
             }
 
@@ -107,7 +163,7 @@ public class SonReceiveAndSendBackService extends BroadcastReceiver {
 
     }
 
-    private Location getLastBestLocation(Context context) {
+    /*private Location getLastBestLocation(Context context) {
 
         if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
@@ -139,6 +195,6 @@ public class SonReceiveAndSendBackService extends BroadcastReceiver {
         }
     }
 
-
+*/
 
 }
